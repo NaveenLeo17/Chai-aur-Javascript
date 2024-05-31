c6b8490517fa707f27c0111419d6b13fc03f782b
Naveen <navstock1967@gmail.com>
2024-05-31 21:24:56 +0530

    complete Datatypes summary file

---
+++ Basics/Datatypes_Summary.js
@@ -0,0 +1,32 @@
+// Primitive
+// 7 types: String, Number, Boolean, null, undefined, symbol, BigInt
+
+// ***** JS is a statically typed language
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+// console.log(id === anotherId); // false
+// console.log(id); // Symbol(123)
+// console.log(anotherId); // Symbol(123)
+
+const bigNumber = 3342332323554432235325325235235n
+const score = null
+
+
+// Non primitive (Reference type) [There return type is object]
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+const myfunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber); // bigint
+console.log(typeof score); // object
+console.log(typeof myfunction); // function (object function)
+console.log(typeof heros); // object 
