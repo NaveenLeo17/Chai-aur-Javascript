813fcd028f373ac3697cc25930e25cf67be2eaed
Naveen <navstock1967@gmail.com>
2024-05-31 17:59:24 +0530

    complete Operations part

--- Basics/conversionOperations.js
+++ Basics/conversionOperations.js
@@ -39,7 +39,35 @@ console.log(booleanIsLoggedIn);
 // ------------------------------------------------------------------ //
 let someNumber = 33
 
-let stirngNumber = String(someNumber)
+let stringNumber = String(someNumber)
 
-console.log(typeof stirngNumber);
-console.log(stirngNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+
+// *************************** Operations *************************** //
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+// console.log(2+3);
+// console.log(2*3);
+// console.log(2**3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+console.log(+true); // 1
+console.log(+""); // 0
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
