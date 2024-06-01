c9a8d77fb9103bef9fcfbbdc52f62d8d3060d2b7
Naveen <navstock1967@gmail.com>
2024-06-01 21:42:26 +0530

    DateAndTime topic

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
